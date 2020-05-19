462804a20512b12762a0d0575159c2b6454136bc
3dformortals <lenivaya10003@mail.ru>
2020-05-19 19:00:30 +0300

    fix index + style changes

--- development/css/switchfont/switchfont.js
+++ development/css/switchfont/switchfont.js
@@ -10,4 +10,15 @@ b.textContent = "RobotoMono-Regular.ttf";
 b.type= "button";
 b.id="fontswitcherbutton";
 b.onclick = switchfont;
+
+b.style.background = "hsla(129, 50%, 0%,1)";
+b.style.color = "hsla(129, 50%, 82%,0.8)";
+b.style.border = "1px solid hsla(180, 50%, 82%,0.8)";
+b.style.borderTop = "0px solid hsla(180, 50%, 82%,0.8)";
+b.style.borderRadius = "0px 0px 25px 25px";
+b.style.paddingLeft = "25px";
+b.style.paddingRight = "25px";
+b.style.paddingBottom = "10px";
+b.style.marginTop = "-10px";
+
 document.body.appendChild(b);
--- js/myeyes.js
+++ js/myeyes.js
@@ -11,4 +11,15 @@ b.textContent = "myeyes >8O";
 b.type= "button";
 b.id="myeyes";
 b.onclick = switchfont;
+
+b.style.background = "hsla(129, 50%, 0%,1)";
+b.style.color = "hsla(129, 50%, 82%,0.8)";
+b.style.border = "1px solid hsla(180, 50%, 82%,0.8)";
+b.style.borderTop = "0px solid hsla(180, 50%, 82%,0.8)";
+b.style.borderRadius = "0px 0px 25px 25px";
+b.style.paddingLeft = "25px";
+b.style.paddingRight = "25px";
+b.style.paddingBottom = "10px";
+b.style.marginTop = "-10px";
+
 document.body.appendChild(b);
